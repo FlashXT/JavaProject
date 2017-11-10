b6a0155a2fe5fb47de0ce60d44cc6a07775eff98
FlashXT <976997551@qq.com>
2017-11-10 11:35:05 +0800

    optimize the fusion images,flashxt,2017.11.10

--- DIP_Project/src/HomeWork2/MainClass.java
+++ DIP_Project/src/HomeWork2/MainClass.java
@@ -23,9 +23,9 @@ public class MainClass extends JPanel{
 //			    e.printStackTrace();
 //		}
 		
-	    JPG jpg1 = new JPG("images\\Fig11.jpg");
-	    JPG jpg2 = new JPG("images\\Fig12.jpg");
-	    String dest = "Figfusion3.jpg";
+	    JPG jpg1 = new JPG("images\\Fig1.jpg");
+	    JPG jpg2 = new JPG("images\\Fig1.jpg");
+	    String dest = "Figfusion.jpg";
 	    Algorithms.ImageFusion(jpg1,jpg2,0.5,0.5,dest);
 	    
 	}
